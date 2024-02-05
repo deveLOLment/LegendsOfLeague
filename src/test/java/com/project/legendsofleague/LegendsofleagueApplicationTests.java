5261d652f34a226e22aa01880657ee78467663ea
sbwoo96a <sbwoo96a@gmail.com>
2024-02-05 17:49:15 +0900

    feat(*):init project

---
+++ src/test/java/com/project/legendsofleague/LegendsofleagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.legendsofleague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendsofleagueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
