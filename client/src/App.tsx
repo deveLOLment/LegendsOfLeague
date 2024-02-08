987dbef3033a02d02c6e9598ad668298c1e09558
hyunwoo0318 <97041290+hyunwoo0318@users.noreply.github.com>
2024-02-08 13:37:55 +0900

    Feat/kakaoPay (#15)
    * feat(*): convert to monorepo
    
    * feat(*): convert to monorepo
    
    * feat(KakaoPay): add backend kakaoPay logic
    
    * feat(KakaoPay): add frontend kakaoPay logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Ex from "./Example/Ex";
+import KakaoPayReady from "./kakaopay/KakaoPayReady";
+import KakaoPayApprove from "./kakaopay/KakaoPayApprove";
+import KakaoPayStart from "./kakaopay/KakaoPayStart";
+import KakaoPaySuccess from "./kakaopay/KakaoPaySuccess";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Ex />}></Route>
+      <Route path="/ex" element={<Ex />}></Route>
+      <Route path="/purchase/kakaoPay" element={<KakaoPayStart />}></Route>
+      <Route
+        path="/purchase/kakaoPay/ready"
+        element={<KakaoPayReady />}
+      ></Route>
+      <Route
+        path="/purchase/kakaoPay/success"
+        element={<KakaoPayApprove />}
+      ></Route>
+      <Route path="purchase/success" element={<KakaoPaySuccess />}></Route>
     </Routes>
   );
 }
---
+++ client/src/kakaopay/KakaoPaySuccess.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const KakaoPaySuccess = () => {
+  return <h2>결제에 성공했습니다!!!!!!</h2>;
+};
+
+export default KakaoPaySuccess;
