829fffb4909441c76a2e7597719c96785defe7a5
JaeniorDeveloper <108642772+Kim-Jaehyun0328@users.noreply.github.com>
2024-02-14 17:35:01 +0900

    Feat/order (#20)
    * feat(README) : add readme (#1)
    
    * feat(Order): order관련 dto 개발
    
    * feat(Order): develop single order logic
    
    * feat(*): delete about generated
    
    ---------
    
    Co-authored-by: hyunwoo0318 <97041290+hyunwoo0318@users.noreply.github.com>

---
+++ server/src/main/java/com/project/legendsofleague/domain/cartItem/dto/CartItemRequestDto.java
@@ -0,0 +1,10 @@
+package com.project.legendsofleague.domain.cartItem.dto;
+
+
+import lombok.Getter;
+
+@Getter
+public class CartItemRequestDto {
+    private Long itemId;
+    private Integer count;
+}
--- server/src/main/java/com/project/legendsofleague/domain/item/service/ItemService.java
+++ server/src/main/java/com/project/legendsofleague/domain/item/service/ItemService.java
@@ -2,31 +2,32 @@ package com.project.legendsofleague.domain.item.service;
 
 import com.project.legendsofleague.domain.item.domain.Item;
 import com.project.legendsofleague.domain.item.domain.ItemImage;
-import com.project.legendsofleague.domain.item.dto.ItemRqDto;
+import com.project.legendsofleague.domain.item.dto.ItemRequestDto;
 import com.project.legendsofleague.domain.item.repository.ItemRepository;
 import com.project.legendsofleague.util.S3Util;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ItemService {
 
-  private final ItemRepository itemRepository;
-  private final S3Util s3Util;
-  private final ItemImageService itemImageService;
+    private final ItemRepository itemRepository;
+    private final S3Util s3Util;
+    private final ItemImageService itemImageService;
 
-  @Transactional
-  public void saveItem(ItemRqDto itemRqDto) {
-    ItemImage tempThumbnailImage = s3Util.saveFile(itemRqDto.getThumbnailImage());
-    String thumbnailImage = tempThumbnailImage.getImageUrl();
-    List<ItemImage> itemImages = s3Util.saveFiles(itemRqDto.getItemImages());
-    Item item = Item.toEntity(itemRqDto, thumbnailImage);
+    @Transactional
+    public void saveItem(ItemRequestDto itemRequestDto) {
+        ItemImage tempThumbnailImage = s3Util.saveFile(itemRequestDto.getThumbnailImage());
+        String thumbnailImage = tempThumbnailImage.getImageUrl();
+        List<ItemImage> itemImages = s3Util.saveFiles(itemRequestDto.getItemImages());
+        Item item = Item.toEntity(itemRequestDto, thumbnailImage);
 
-    itemRepository.save(item);
-    itemImageService.saveItemImage(itemImages, item);
-  }
+        itemRepository.save(item);
+        itemImageService.saveItemImage(itemImages, item);
+    }
 }
