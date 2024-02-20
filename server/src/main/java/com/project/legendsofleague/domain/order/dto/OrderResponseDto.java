1edb536e144cf13aab95467b2c3765a0324d4702
rlawwogus@ajou.ac.kr <rlawwogus@ajou.ac.kr>
2024-02-20 13:26:25 +0900

    refactor(Order): refactor orderResponseDto and orderService about detailOrderPage method

--- server/src/main/java/com/project/legendsofleague/domain/order/dto/OrderResponseDto.java
+++ server/src/main/java/com/project/legendsofleague/domain/order/dto/OrderResponseDto.java
@@ -3,11 +3,12 @@ package com.project.legendsofleague.domain.order.dto;
 import com.project.legendsofleague.domain.membercoupon.dto.MemberCouponResponseDto;
 import com.project.legendsofleague.domain.order.domain.Order;
 import com.project.legendsofleague.domain.order.domain.OrderItem;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Slf4j
@@ -18,8 +19,15 @@ public class OrderResponseDto {
     private List<OrderItemResponseDto> orderItemList = new ArrayList<>();
 
 
+    /**
+     * 주문하기를 눌렀을 때 Order에 적용할 쿠폰 등을 체크하기 위한 정적  팩토리 메소드
+     *
+     * @param orderItems
+     * @param couponResponseList
+     * @return
+     */
     public static OrderResponseDto toDto(List<OrderItem> orderItems,
-        Map<Long, List<MemberCouponResponseDto>> couponResponseList) {
+                                         Map<Long, List<MemberCouponResponseDto>> couponResponseList) {
         OrderResponseDto orderResponseDto = new OrderResponseDto();
 
         Order order = orderItems.get(0).getOrder(); //매개변수로 온 orderItems의 order는 모두 같은 order이다.
