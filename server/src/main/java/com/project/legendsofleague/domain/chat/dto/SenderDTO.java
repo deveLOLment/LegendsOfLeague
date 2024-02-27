5006ce6c055c638bf72a856b742d979f035cfead
gumgu <bienlee@naver.com>
2024-02-27 17:31:14 +0900

    feat(Chat/*): 채팅 기능 구현

---
+++ server/src/main/java/com/project/legendsofleague/domain/chat/dto/SenderDTO.java
@@ -0,0 +1,13 @@
+package com.project.legendsofleague.domain.chat.dto;
+
+import lombok.Data;
+
+/**
+ * 채팅 서버에 접속한 사용자의 아이디를 담는 DTO 입니다.
+ */
+@Data
+public class SenderDTO {
+
+    private String username; // 사용자 이름
+
+}
