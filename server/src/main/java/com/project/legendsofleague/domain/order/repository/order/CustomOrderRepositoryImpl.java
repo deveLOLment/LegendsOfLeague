01a5de2649d388a645b6fa346b72593feb09bdd0
hyunwoo <hyunwoo0318@naver.com>
2024-02-28 20:46:55 +0900

    fix(itemReview): fix itemReview bound

--- server/src/main/java/com/project/legendsofleague/domain/order/repository/order/CustomOrderRepositoryImpl.java
+++ server/src/main/java/com/project/legendsofleague/domain/order/repository/order/CustomOrderRepositoryImpl.java
@@ -1,20 +1,16 @@
 package com.project.legendsofleague.domain.order.repository.order;
 
 
+import static com.project.legendsofleague.domain.member.domain.QMember.member;
+import static com.project.legendsofleague.domain.order.domain.QOrder.order;
+import static com.project.legendsofleague.domain.purchase.domain.QPurchase.purchase;
+
 import com.project.legendsofleague.domain.order.domain.OrderStatus;
 import com.project.legendsofleague.domain.purchase.domain.Purchase;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-import static com.project.legendsofleague.domain.item.domain.QItem.item;
-import static com.project.legendsofleague.domain.member.domain.QMember.member;
-import static com.project.legendsofleague.domain.order.domain.QOrder.order;
-import static com.project.legendsofleague.domain.order.domain.QOrderItem.orderItem;
-import static com.project.legendsofleague.domain.purchase.domain.QPurchase.purchase;
 
 @Repository
 @RequiredArgsConstructor
@@ -27,7 +23,7 @@ public class CustomOrderRepositoryImpl implements CustomOrderRepository {
         return queryFactory.selectFrom(purchase).distinct()
                 .leftJoin(purchase.order, order).fetchJoin()
                 .leftJoin(order.orderItemList).fetchJoin()
-                .leftJoin(orderItem.item, item).fetchJoin()
+//                .leftJoin(orderItem.item, item).fetchJoin()
                 .leftJoin(order.member, member).fetchJoin()
                 .where(order.member.id.eq(memberId)
                         .and(order.orderStatus.eq(OrderStatus.SUCCESS)
