b1b832cc249859cb5e1d9eb64a06a9eaba30403d
hyunwoo0318 <97041290+hyunwoo0318@users.noreply.github.com>
2024-03-05 16:04:40 +0900

    Feat/item review (#49)
    * feat(itemReview, itemComment): add itemReview, itemComment feature
    
    * feat(itemReview, itemComment): add itemReview, itemComment feature
    
    * fix(itemReview): fix itemReview bound
    
    * fix(itemReview): fix itemReview bound
    
    * feat(*): feat front logic
    
    * feat(refund): feat order refund
    
    * feat(review): add review score validation

--- server/src/main/java/com/project/legendsofleague/domain/itemreview/dto/ItemReviewCreateRequestDto.java
+++ server/src/main/java/com/project/legendsofleague/domain/itemreview/dto/ItemReviewCreateRequestDto.java
@@ -1,11 +1,17 @@
 package com.project.legendsofleague.domain.itemreview.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.Getter;
 
 @Getter
 public class ItemReviewCreateRequestDto {
 
+    @Min(1)
+    @Max(5)
     private Integer score;
+
+
     private String content;
 
 }
