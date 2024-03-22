d0c202902a7a4860d1d053aabc2f5d3b7327288f
venus01 <102720261+Venus1234567@users.noreply.github.com>
2024-03-22 16:24:20 +0900

    Feat/add member swagger (#88)
    * feat(set-cookie) : 로그인 성공시 쿠키 발급 방식 변경
    
    * feat(set-cookie) : 로그인 성공시 쿠키 발급 방식 변경
    
    * Revert "feat(set-cookie) : 로그인 성공시 쿠키 발급 방식 변경"
    
    This reverts commit a145e612f95d219a646aff7d688cb58e2737746c.
    
    * feat(add-member-swagger) : @Schema 어노테이션을 통해 Member 엔티티 관한 스키마 설명 추가

--- server/src/main/java/com/project/legendsofleague/domain/member/domain/Member.java
+++ server/src/main/java/com/project/legendsofleague/domain/member/domain/Member.java
@@ -3,6 +3,7 @@ package com.project.legendsofleague.domain.member.domain;
 import com.project.legendsofleague.common.BaseEntity;
 import com.project.legendsofleague.domain.member.dto.OAuth2Response;
 import com.project.legendsofleague.domain.member.dto.RegisterDto;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -24,25 +25,31 @@ public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
+    @Schema(name = "Member ID", example = "1")
     Long id;
 
     // 회원 비밀번호
+    @Schema(name = "Password", example = "password123")
     String password;
 
     // 로그인용 아이디
     // 스프링 시큐리티에서는 username, password를 통해 로그인을 하기 때문에 username이 중복되지 않게 unique 키로 설정한다.
     @Column(unique = true)
+    @Schema(name = "Username", example = "exam")
     String username;
 
     // 닉네임
     @Column(unique = true)
+    @Schema(name = "Nickname", example = "venus")
     String nickname;
 
     // 회원 이메일
+    @Schema(name = "Email", example = "exam@naver.com")
     String email;
 
     // 회원 타입(일반회원, 관리자)
     @Enumerated(EnumType.STRING)
+    @Schema(name = "Role", example = "ROLE_USER")
     ROLE role;
 
 //    String role;
