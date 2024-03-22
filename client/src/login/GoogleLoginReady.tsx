f3f1280440600f35a2ed842b16a6dd97bd904a86
venus01 <geumsung7769@naver.com>
2024-03-22 15:14:57 +0900

    feat(add-member-swagger) : @Schema 어노테이션을 통해 Member 엔티티 관한 스키마 설명 추가

--- client/src/login/GoogleLoginReady.tsx
+++ client/src/login/GoogleLoginReady.tsx
@@ -1,46 +1,57 @@
-import React from "react";
+import React from 'react'
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
+
+
 
 const googleLoginReady = () => {
-  const onNaverLogin = () => {
+  const onGoogleLogin = () => {
     window.location.href =
       "https://15.164.169.174.nip.io/oauth2/authorization/google";
 
-    // alert(document.cookie);
+// alert(document.cookie);
 
-    // 쿠키에서 토큰 꺼내오기
+// 쿠키에서 토큰 꺼내오기
 
-    // const token = document.cookie
-    //     .split('; ')
-    //     .find(row => row.startsWith('Authorization'))
-    //     .split("=")[1]
-    //     .replace(/\+/g, ' ');
-    // const token = document.cookie
-    //     .split('; ')
-    //     .find(row => row.startsWith('Authorization'))
-    //     ?.split('=')[1]?.replace(/\+/g, ' ')
-    //     ?? '기본값';
+// const token = document.cookie
+//     .split('; ')
+//     .find(row => row.startsWith('Authorization'))
+//     .split("=")[1]
+//     .replace(/\+/g, ' ');
+// const token = document.cookie
+//     .split('; ')
+//     .find(row => row.startsWith('Authorization'))
+//     ?.split('=')[1]?.replace(/\+/g, ' ')
+//     ?? '기본값';
 
-    // alert(token);
+// alert(token);
 
-    // // 토큰 저장
-    // localStorage.setItem("access_token", token);
+// // 토큰 저장
+// localStorage.setItem("access_token", token);
 
-    // // 토큰 만료 시간 설정
-    // const expirationTime = Date.now() + 3600 * 1000; // 현재 시간에 1시간을 더함
+// // 토큰 만료 시간 설정
+// const expirationTime = Date.now() + 3600 * 1000; // 현재 시간에 1시간을 더함
 
-    // // 토큰 만료 시간 저장
+// // 토큰 만료 시간 저장
 
-    // localStorage.setItem('tokenExpiration', expirationTime.toString());
-  };
+// localStorage.setItem('tokenExpiration', expirationTime.toString());
 
-  return (
-    <div>
-      googleLoginReady        {" "}
-      <button onClick={onNaverLogin}>googleLogin</button>   {" "}
-    </div>
-  );
-};
 
-export default googleLoginReady;
+    }
+
+  return (
+
+    <div>googleLoginReady
+
+        <button onClick={onGoogleLogin}>googleLogin</button>
+
+    </div>
+
+  )
+
+}
+
+
+
+export default googleLoginReady
